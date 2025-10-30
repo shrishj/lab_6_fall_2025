d5feac0dde89d1bbe09cf8e442dc54c2909a6edf
shrishj <148286466+shrishj@users.noreply.github.com>
2025-10-30 14:15:55 -0700

    dance

--- pupper_llm/karel/karel.py
+++ pupper_llm/karel/karel.py
@@ -229,6 +229,9 @@ class KarelPupper:
         # Be creative and choreograph the most exciting dance possible!
         self.wiggle()
         self.turn_left()
+        self.turn_left()
+        self.turn_left()
+        self.turn_right()
         self.turn_right()
         self.bob()
         self.stop()
